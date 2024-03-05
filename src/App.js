3a92dc07d8a7b9f849fcd75ed5d8ae320fa9d5c0
Beens von Benis Kowalikov <145481307+BeensVonBenis@users.noreply.github.com>
2024-03-05 13:57:09 +0100

    Granie dźwięków i interwały

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
+import * as Tone from "tone";
 
 let intervalid = "";
 let progresja = [];
 let metrum = 0;
 let grany = 0;
+
 function App() {
+  const synth = new Tone.Synth().toDestination();
   const dzwieki = [
     "a",
     "a#",
@@ -20,12 +23,35 @@ function App() {
     "g",
     "g#",
   ];
-  const struny = ["e", "a", "d", "g", "b", "e"];
+  const interwaly = [
+    "1cz",
+    "2m",
+    "2w",
+    "3m",
+    "3w",
+    "4cz",
+    "5zm",
+    "5cz",
+    "6m",
+    "6w",
+    "7m",
+    "7w",
+  ];
+  const struny = [
+    ["e", 2],
+    ["a", 2],
+    ["d", 3],
+    ["g", 3],
+    ["b", 3],
+    ["e", 4],
+  ];
   const modulacje = {
     m7: [0, 2, 3, 7, 10],
     maj7: [0, 2, 4, 7, 11],
     7: [0, 2, 4, 7, 10],
     mb57: [0, 1, 3, 6, 10],
+    mol: [0, 3, 5, 7, 10],
+    dur: [0, 2, 4, 7, 9],
   };
   const [dzwAkordowy, setDzwAkordowy] = useState("a");
   const [modAkordowy, setModAkordowy] = useState("m7");
@@ -33,6 +59,8 @@ function App() {
   const [pokazujGryf, setPokazujGryf] = useState(true);
   const [pokazujGrane, setPokazujGrane] = useState(true);
   const [pokazujPryme, setPokazujPryme] = useState(false);
+  const [pokazujInterwaly, setPokazujInterwaly] = useState(false);
+  const [grajDzwieki, setGrajDzwieki] = useState(true);
   const [metronom, setMetronom] = useState(metrum);
   const [trafione, setTrafione] = useState(null);
   function dzwiekiEtiudy() {
@@ -54,7 +82,6 @@ function App() {
   function metronomClick() {
     setMetronom(((metrum + 1) % 4) + 1);
     metrum++;
-    console.log(metrum);
     if (metrum === 4) {
       metrum = 0;
 
@@ -63,17 +90,22 @@ function App() {
       } else {
         grany = 0;
       }
-      if (progresja[grany].includes("#")) {
-        setDzwAkordowy(progresja[grany][0] + "#");
-        setModAkordowy(...progresja[grany].slice(2));
-      } else {
-        setDzwAkordowy(...progresja[grany][0]);
-        setModAkordowy(progresja[grany].slice(1));
+      if (progresja.length === 1) {
+        if (progresja[grany].includes("#")) {
+          setDzwAkordowy(progresja[grany][0] + "#");
+          setModAkordowy(...progresja[grany].slice(2));
+        } else {
+          setDzwAkordowy(...progresja[grany][0]);
+          setModAkordowy(progresja[grany].slice(1));
+        }
       }
     }
   }
   async function grajProg() {
-    progresja = progRef.current.value.split(" ").map((a) => a.trim());
+    progresja = progRef.current.value
+      .split(" ")
+      .map((a) => a.trim())
+      .map((a) => a.toLowerCase());
     const delay = (60 * 1000) / bpmRef.current.value;
     clearInterval(intervalid);
     if (delay === Infinity) {
@@ -82,15 +114,18 @@ function App() {
       intervalid = setInterval(metronomClick, delay);
     }
   }
-  function grajSolo(dzwiek) {
+  function grajSolo(dzwiek, oktawa) {
     metronomClick();
+    if (grajDzwieki) {
+      synth.triggerAttackRelease(`${dzwiek}${oktawa}`, `8n`);
+    }
+
     if (ogrywane.includes(dzwiek)) {
       setTrafione(true);
     } else {
       setTrafione(false);
     }
   }
-  console.log(trafione);
   return (
     <div
       className="App"
@@ -118,6 +153,8 @@ function App() {
           <option value={"maj7"}>maj7</option>
           <option value={"7"}>7</option>
           <option value={"mb57"}>mb57</option>
+          <option value={"mol"}>mol</option>
+          <option value={"dur"}>dur</option>
         </select>
       </div>
       <div>
@@ -126,6 +163,13 @@ function App() {
         <button onClick={() => grajProg()}>Zatwierdź</button>
       </div>
       <div className="opcje">
+        <label>Graj dźwięki</label>
+        <input
+          onClick={() => setGrajDzwieki((a) => !a)}
+          type="checkbox"
+          defaultChecked
+        ></input>
+        <br></br>
         <label>Pokazuj dźwięki na gryfie</label>
         <input
           defaultChecked
@@ -147,6 +191,12 @@ function App() {
           type="checkbox"
         ></input>
         <br></br>
+        <label>Pokazuj interwały</label>
+        <input
+          onClick={() => setPokazujInterwaly((a) => !a)}
+          type="checkbox"
+        ></input>
+        <br></br>
       </div>
       <br></br>
       <Progresja metronom={metronom} grany={grany}></Progresja>
@@ -160,12 +210,15 @@ function App() {
         <div className="gryf">
           {struny.toReversed().map((a) => (
             <Struna
-              nuta={a}
+              nuta={a[0]}
+              oktawa={a[1]}
               dzwieki={dzwieki}
+              interwaly={interwaly}
               ogrywane={ogrywane}
               pokazujGryf={pokazujGryf}
               pokazujPryme={pokazujPryme}
               pokazujGrane={pokazujGrane}
+              pokazujInterwaly={pokazujInterwaly}
               metronomClick={metronomClick}
               grajSolo={grajSolo}
             ></Struna>
@@ -178,31 +231,43 @@ function App() {
 function Struna(props) {
   const {
     nuta,
+    oktawa,
     dzwieki,
+    interwaly,
     ogrywane,
     pokazujGryf,
     pokazujGrane,
     pokazujPryme,
+    pokazujInterwaly,
     grajSolo,
   } = props;
   let dzwStruny = dzwieki
     .concat(dzwieki)
     .slice(dzwieki.findIndex((a) => a === nuta));
   dzwStruny = dzwStruny.slice(0, dzwieki.length + 1);
+  const nowaOktawa = dzwStruny.findIndex((a) => a === "c");
+  const pryma = dzwStruny.findIndex((a) => a === ogrywane[0]);
   return (
     <div className="struna">
-      {dzwStruny.map((a) => (
-        <span
-          onClick={() => grajSolo(a)}
-          className={"dzwiek " + (pokazujPryme && a === ogrywane[0] && "pryma")}
-          style={{
-            backgroundColor:
-              ogrywane.includes(a) && pokazujGrane ? "red" : "white",
-          }}
-        >
-          {pokazujGryf ? a : "⠀"}
-        </span>
-      ))}
+      {dzwStruny.map((a, i) => {
+        return (
+          <span
+            onClick={() => grajSolo(a, oktawa + (i >= nowaOktawa))}
+            className={
+              "dzwiek " + (pokazujPryme && a === ogrywane[0] && "pryma")
+            }
+            style={{
+              backgroundColor:
+                ogrywane.includes(a) && pokazujGrane ? "red" : "white",
+            }}
+          >
+            {pokazujInterwaly && (
+              <div className="interwal">{interwaly[(i - pryma + 12) % 12]}</div>
+            )}
+            {pokazujGryf ? a : "⠀"}
+          </span>
+        );
+      })}
     </div>
   );
 }
@@ -219,7 +284,6 @@ function Metronom(props) {
 
 function Progresja(props) {
   const { grany } = props;
-  console.log(grany);
   return (
     <div>
       {progresja.map((a, b) => (
